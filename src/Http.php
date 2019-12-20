59601d6e0a5a42d8f38100b5b887e43c3684bf0f
seekwe <soseek@vip.qq.com>
2019-12-20 15:42:43 +0800

    fix: set header

--- src/Http.php
+++ src/Http.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace Zls\Swoole;
 
-use Exception;
 use Swoole\Http\Request;
 use swoole\Http\Response;
 use swoole\Server;
@@ -23,16 +22,13 @@ class Http
      * @param array      $config
      *
      * @return string
-     * @throws Exception
      */
     public function onRequest($request, $response, $server, $zlsConfig, $config = []): string
     {
         Z::di()->bind(SWOOLE_RESPONSE, static function () use ($response) {
             return $response;
         });
-        /** @noinspection PhpUndefinedFieldInspection */
         $_SERVER = array_change_key_case($request->server, CASE_UPPER);
-        /** @noinspection PhpUndefinedFieldInspection */
         $_HEADER = array_change_key_case($request->header, CASE_UPPER);
         $_GET    = $request->get ?? [];
         $_POST   = $request->post ?? [];
@@ -42,7 +38,6 @@ class Http
             $_SERVER['HTTP_' . str_replace('-', '_', $key)] = $value;
         }
         $_SERVER['REMOTE_ADDR'] = Z::arrayGet($_SERVER, 'REMOTE_ADDR', Z::arrayGet($_HEADER, 'REMOTE_ADDR', Z::arrayGet($_HEADER, 'X-REAL-IP')));
-        /** @noinspection PhpVoidFunctionResultUsedInspection */
         $_SERVER['ZLS_POSTRAW'] = $request->rawContent();
         $pathInfo               = Z::arrayGet($_SERVER, 'PATH_INFO');
         $_SERVER['PATH_INFO']   = $pathInfo;
@@ -55,12 +50,12 @@ class Http
             'cookie'  => $_COOKIE ?? [],
             'session' => $_SESSION ?? [],
         ], ZLS_PREFIX);
-        /** @noinspection PhpUndefinedMethodInspection */
         $zlsConfig->setAppDir(ZLS_APP_PATH)->getRequest()->setPathInfo($pathInfo);
         if (Z::arrayGet($config, 'watch') && '1' === Z::arrayGet($_GET, '_reload')) {
             $this->printLog('reload Serve');
             $server->reload();
         }
+        Z::setGlobalData(ZLS_PREFIX . 'setHeader', []);
         ob_start();
         try {
             $zlsConfig->bootstrap();
@@ -71,20 +66,17 @@ class Http
             echo $e->getMessage();
         }
         $content = ob_get_clean();
-        Z::defer(function () use ($response) {
-            $headers = Z::getGlobalData(ZLS_PREFIX . 'setHeader', []);
-            foreach ($headers as $header) {
-                $header = explode(':', $header);
-                $k      = array_shift($header);
-                $c      = join(':', $header);
-                $response->header($k, trim($c));
-            }
-            $cookies = Z::getGlobalData(ZLS_PREFIX . 'setCookie', []);
-            foreach ($cookies as $cookie) {
-                $response->cookie(...$cookie);
-            }
-        });
-        $zlsConfig->bootstrap();
+        $headers = Z::getGlobalData(ZLS_PREFIX . 'setHeader', []);
+        foreach ($headers as $header) {
+            $header = explode(':', $header);
+            $k      = array_shift($header);
+            $c      = join(':', $header);
+            $response->header($k, trim($c));
+        }
+        $cookies = Z::getGlobalData(ZLS_PREFIX . 'setCookie', []);
+        foreach ($cookies as $cookie) {
+            $response->cookie(...$cookie);
+        }
         Z::eventEmit(ZLS_PREFIX . 'DEFER');
         Z::resetZls();
 
